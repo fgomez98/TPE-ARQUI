5d4a1aff2e4896fab6a29eda12fd474a8eaeabd5
pvedoya <33105601+pvedoya@users.noreply.github.com>
2018-05-24 10:13:56 -0300

    Add files via upload

---
+++ Userland/SampleCodeModule/TimeModule.c
@@ -0,0 +1,29 @@
+//
+//  TimeModule.c
+//  
+//
+//  Created by Fermin Gomez on 5/22/18.
+//
+//
+
+#include <stdio.h>
+// Donde van los modulos
+// Definir HOTKEY
+// Incluir los .h
+// Cuando corta?? lo hace el shell
+/*
+void initializeHour(){
+    int colours[5] = {};
+    int colour = colours[0];
+    int i = 0;
+    
+    while(true){
+        char * currTime = getTime();
+        if(keyPressed() && getKey() = HOTKEY){
+            colour = colours[++i];
+            i= i%6;
+        }
+        showTime(colour, currTime);
+    }
+}
+*/
